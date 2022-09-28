00fa090ef4755c44f7d33ca4d9c3cd2130dd5619
umudAbbasli <108527234+umudAbbasli@users.noreply.github.com>
2022-09-28 19:33:43 +0400

    changes

--- js/App.js
+++ js/App.js
@@ -1,26 +1,28 @@
 $(document).ready(function(){
     $(".card-1").mouseover(function(){
-        $(".xidmetlerimiz-cards").css({"background":"url(../img/tls-slide-1-img-1.jpg) center/cover"})
+        $(".xidmetlerimiz-cards").css({"background":"url(../img/DƏMİR-YOLU-DAŞINMASI.jpg) center/cover"})
     })
     $(".card-2").mouseover(function(){
-        $(".xidmetlerimiz-cards").css({"background":"url(../img/tls-slide-1-img-2.jpg) center/cover"})
+        $(".xidmetlerimiz-cards").css({"background":"url(../img/DƏNİZ-YOLU-DAŞINMASI.jpg) center/cover"})
     })
     $(".card-3").mouseover(function(){
-        $(".xidmetlerimiz-cards").css({"background":"url(../img/tls-slide-1-img-3.jpg) center/cover"})
+        $(".xidmetlerimiz-cards").css({"background":"url(../img/GÖMRÜK-RƏSMİLƏŞDİRİLMƏSİ-VƏ-BROKER-XİDMƏTİ.jpg) center/cover"})
     })
     $(".card-4").mouseover(function(){
-        $(".xidmetlerimiz-cards").css({"background":"url(../img/tls-slide-1-img-4.jpg) center/cover"})
+        $(".xidmetlerimiz-cards").css({"background":"url(../img/HAVA-YOLU-DAŞINMASI.png) center/cover"})
     })
     $(".card-5").mouseover(function(){
-        $(".xidmetlerimiz-cards").css({"background":"url(../img/tls-slide-1-img-1.jpg) center/cover"})
+        $(".xidmetlerimiz-cards").css({"background":"url(../img/QURU-YOL-DAŞINMASI.png) center/cover"})
     })
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         if (scroll > 70) {
-            $('.navbar-my-bg').css({"background":"#fff"});  
+            $('.navbar-my-bg').css({"background":"rgba(255, 3, 3, 0.823)"});  
+            $('.navbar-button > button').css({"background":"#fff","color":"#dd3333"})
 		  }
           else{
             $('.navbar-my-bg').css({"background":"transparent"});  
+            $('.navbar-button > button').css({"background":"#dd3334","color":"#fff"})
           }
 
     })
